ae09e8b056ab1452d4d1b5281a8b921727c0c4e0
Fredi <fjaramillo@steelcloud.com>
2013-02-22 13:53:45 -0500

    First commit

---
+++ STIGDocument/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace STIGDocument
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInputDocu = new System.Windows.Forms.TextBox();
+            this.txtOutputDocu = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBreakingString = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtGroupIDCount = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblori = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(267, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input File:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Output File:";
+            // 
+            // txtInputDocu
+            // 
+            this.txtInputDocu.Location = new System.Drawing.Point(107, 27);
+            this.txtInputDocu.Name = "txtInputDocu";
+            this.txtInputDocu.Size = new System.Drawing.Size(185, 20);
+            this.txtInputDocu.TabIndex = 3;
+            this.txtInputDocu.Text = "c:\\borre\\inputfile.txt";
+            // 
+            // txtOutputDocu
+            // 
+            this.txtOutputDocu.Location = new System.Drawing.Point(107, 56);
+            this.txtOutputDocu.Name = "txtOutputDocu";
+            this.txtOutputDocu.Size = new System.Drawing.Size(185, 20);
+            this.txtOutputDocu.TabIndex = 4;
+            this.txtOutputDocu.Text = "c:\\borre\\outputFile.txt";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Breaking String:";
+            // 
+            // txtBreakingString
+            // 
+            this.txtBreakingString.Location = new System.Drawing.Point(107, 96);
+            this.txtBreakingString.Name = "txtBreakingString";
+            this.txtBreakingString.Size = new System.Drawing.Size(235, 20);
+            this.txtBreakingString.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Groud ID count:";
+            // 
+            // txtGroupIDCount
+            // 
+            this.txtGroupIDCount.Location = new System.Drawing.Point(107, 128);
+            this.txtGroupIDCount.Name = "txtGroupIDCount";
+            this.txtGroupIDCount.Size = new System.Drawing.Size(100, 20);
+            this.txtGroupIDCount.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(104, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Generates text file";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(104, 208);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Generates XML file";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 265);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 20);
+            this.textBox1.TabIndex = 14;
+            this.textBox1.Text = "c:\\borre\\outputFile.xml";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(107, 236);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 20);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "c:\\borre\\inputfile.txt";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 267);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Output File:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(22, 238);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Input File:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(267, 308);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Run";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblori
+            // 
+            this.lblori.AutoSize = true;
+            this.lblori.Location = new System.Drawing.Point(28, 188);
+            this.lblori.Name = "lblori";
+            this.lblori.Size = new System.Drawing.Size(20, 13);
+            this.lblori.TabIndex = 17;
+            this.lblori.Text = "Ori";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(25, 158);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "label9";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 342);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lblori);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtGroupIDCount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBreakingString);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOutputDocu);
+            this.Controls.Add(this.txtInputDocu);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInputDocu;
+        private System.Windows.Forms.TextBox txtOutputDocu;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBreakingString;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtGroupIDCount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblori;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ STIGDocument/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace STIGDocument
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputFile = txtInputDocu.Text;
+            string outputFile = txtOutputDocu.Text;
+            ReadAndWriteTextFile(inputFile, outputFile);
+            int count = CountGroupID(inputFile);
+            txtGroupIDCount.Text = count.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string inputFile = textBox2.Text;
+            string outputFile = textBox1.Text;
+            GenerateXMLFile(inputFile, outputFile);
+            MessageBox.Show("Done");
+        }
+
+        private void ReadAndWriteTextFile(string inputFile, string outputFile)
+        {
+            List<string> titles = new List<string>();
+            titles.Add("Group ID (Vulid):");
+            titles.Add("Group Title:");
+            titles.Add("Rule ID:");
+            titles.Add("Severity:");
+            titles.Add("Rule Version (STIG-ID):");
+            titles.Add("Rule Title:");
+            titles.Add("Vulnerability Discussion:");
+            titles.Add("Responsibility:");
+            titles.Add("IAControls:");
+            titles.Add("Check Content:");
+            titles.Add("Fix Text:");
+            //
+            //
+            StreamReader re = File.OpenText(inputFile);
+            StreamWriter ro = new StreamWriter(outputFile);
+            string input = null;
+            string outputLine = "";
+            bool lineBreak = false;
+            while ((input = re.ReadLine()) != null)
+            {
+                if (input.Length > 0)
+                {
+                    if (input.StartsWith(txtBreakingString.Text))
+                    {
+                        ro.WriteLine(outputLine);
+                        outputLine = "";
+                        lineBreak = true;
+                        continue;
+                    }
+                    bool found = false;
+                    foreach (string title in titles)
+                    {
+                        if (input.StartsWith(title))
+                        {
+                            if (lineBreak)
+                            {
+                                lineBreak = false;
+                            }
+                            else
+                            {
+                                outputLine = outputLine + "|";
+                            }
+                            outputLine = outputLine + input.Substring(input.IndexOf(":") + 1, input.Length - input.IndexOf(":") - 1);
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found)
+                    {
+                        continue;
+                    }
+                    outputLine = outputLine + input;
+                }
+            }
+            re.Close();
+            ro.Close();
+            MessageBox.Show("Done");
+        }
+
+        private int CountGroupID(string inputFile)
+        {
+            StreamReader re = File.OpenText(inputFile);
+            string input = null;
+            int count = 0;
+            while ((input = re.ReadLine()) != null)
+            {
+                if (input.Length > 0 && input.StartsWith("Group ID (Vulid):"))
+                {
+                    count++;
+                }
+            }
+            re.Close();
+            return count;
+        }
+
+
+        private void GenerateXMLFile(string inputFile, string outputFile)
+        {
+            List<string> tabs = new List<string>();
+            tabs.Add("GroupId");
+            tabs.Add("GroupTitle");
+            tabs.Add("RuleId");
+            tabs.Add("Severity");
+            tabs.Add("RuleVersion");
+            tabs.Add("RuleTitle");
+            tabs.Add("Where");
+            tabs.Add("Applied");
+            tabs.Add("Type");
+            tabs.Add("Value");
+            tabs.Add("Ignore");
+            tabs.Add("IgnoreReason");
+            //
+            //
+            StreamReader re = File.OpenText(inputFile);
+            StreamWriter ro = new StreamWriter(outputFile);
+            ro.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            ro.WriteLine("<Groups>");
+            string input = null;
+            while ((input = re.ReadLine()) != null)
+            {
+                ro.WriteLine("  <Group>");
+                int columnsCount = 0;
+                string[] columns = input.Split('\t');
+                foreach (string column in columns)
+                {
+                    ro.WriteLine("    <" + tabs[columnsCount] + ">" + column + "</" + tabs[columnsCount] + ">");
+                    columnsCount++;
+                }
+                //
+                for (int i = 7; i <= 11; i++)
+                {
+                    ro.WriteLine("    <" + tabs[i] + ">" + "</" + tabs[i] + ">");
+                }
+                ro.WriteLine("  </Group>");
+            }
+            ro.WriteLine("</Groups>");
+            re.Close();
+            ro.Close();
+        }
+    }
+}
